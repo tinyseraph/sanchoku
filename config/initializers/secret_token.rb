c96ec46c4c40aacadd376154e799c62157f398a3
Yasuhiro Ota <ya-ota@mfeed.ad.jp>
2013-08-22 09:57:53 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sanchoku::Application.config.secret_key_base = 'bbc19cb3d1bc3392ad8d3a5972ed6ba9d259d5814eb5fec25e8f03cf45aa8f45959a11460425d0d47dbc3685bdf9160b057b5b637eb6470b90e1b4f93f7dadef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sanchoku::Application.config.session_store :cookie_store, key: '_sanchoku_session'
